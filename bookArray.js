6a9274b9654faa786be65dd97bfef50731c8fcdd
victor chris <bigsmart2026@gmail.com>
2026-03-27 12:21:41 +0100

    initial commit

---
+++ bookArray.js
@@ -0,0 +1,13 @@
+function printBookDetails() {
+    let book = {
+        title: "JavaScript ArrayOfObject",
+        pages: 250,
+        isAvailable: true
+    };
+
+    console.log("Title:", book.title);
+    console.log("Pages:", book.pages);
+    console.log("Available:", book.isAvailable);
+}
+
+printBookDetails();
